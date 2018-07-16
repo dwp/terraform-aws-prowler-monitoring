911ba5246e069e8bbcadddb2ee767eef6fe47d6c
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2018-07-16 17:14:49 +0100

    Terraform module that creates a collection of AWS log metric filters and alarms to satisfy section 3 of Prowler

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# terraform-aws-prowler-monitoring
-A collection of log metric filters and alarms to satisfy Prowler Monitoring chacks
+# AWS Prowler monitoring Terraform module
+Terraform module that creates a collection of AWS log metric filters and alarms to satisfy section 3 of the [Prowler Monitoring](https://github.com/toniblyx/prowler) checks. This modules uses the [AWS metric filter alarm](https://registry.terraform.io/modules/dwp/metric-filter-alarm/aws) Terraform module.
+
+## Usage
+```hcl
+module "my_prowler_monitoring" {
+  source = "dwp/prowler-monitoring/aws"
+  
+  log_group_name = "MyLogGroup"
+}
+```
+
+## Examples
+The following example creates a CloudWatch Log Group, SNS Topic and alarms to satisfy section 3 of the Prowler monitoring checks.
+If any of the alarms go into an "ALARM" state a notification will be sent to the SNS Topic.
+```hcl
+
+resource "aws_cloudwatch_log_group" "MyLogGroup" {
+  name = "MyLogGroup"
+}
+
+resource "aws_sns_topic" "MyTopic" {
+  name = "MyTopic"
+  display_name = "My Topic"
+}
+
+module "my_prowler_monitoring" {
+  source = "dwp/terraform-aws-prowler-monitoring"
+  
+  log_group_name = "${aws_cloudwatch_log_group.MyLogGroup.name}"
+  alarm_action_arns = ["${aws_sns_topic.MyTopic.arn}"]
+}
+```
---
+++ main.tf
@@ -0,0 +1,153 @@
+module "metric_filter_alarm_3_1" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.errorCode = \"*UnauthorizedOperation\") || ($.errorCode = \"AccessDenied*\") }"
+  alarm_name = "3.1 Unauthorized API calls"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_2" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ $.eventName = \"ConsoleLogin\" && $.additionalEventData.MFAUsed = \"No\" }"
+  alarm_name = "3.2 Management Console sign-in without MFA"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_3" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ $.userIdentity.type = \"Root\" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != \"AwsServiceEvent\" }"
+  alarm_name = "3.3 usage of root account"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_4" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{($.eventName=DeleteGroupPolicy)||($.eventName=DeleteRolePolicy)||($.eventName=DeleteUserPolicy)||($.eventName=PutGroupPolicy)||($.eventName=PutRolePolicy)||($.eventName=PutUserPolicy)||($.eventName=CreatePolicy)||($.eventName=DeletePolicy)||($.eventName=CreatePolicyVersion)||($.eventName=DeletePolicyVersion)||($.eventName=AttachRolePolicy)||($.eventName=DetachRolePolicy)||($.eventName=AttachUserPolicy)||($.eventName=DetachUserPolicy)||($.eventName=AttachGroupPolicy)||($.eventName=DetachGroupPolicy)}"
+  alarm_name = "3.4 IAM policy changes"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_5" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventName = CreateTrail) || ($.eventName = UpdateTrail) || ($.eventName = DeleteTrail) || ($.eventName = StartLogging) || ($.eventName = StopLogging) }"
+  alarm_name = "3.5 CloudTrail configuration changes"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_6" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventName = ConsoleLogin) && ($.errorMessage = \"Failed authentication\") }"
+  alarm_name = "3.6 AWS Management Console authentication failures"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_7" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventSource = kms.amazonaws.com) && (($.eventName=DisableKey) || ($.eventName=ScheduleKeyDeletion)) }"
+  alarm_name = "3.7 disabling or scheduled deletion of customer created CMKs"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_8" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventSource = s3.amazonaws.com) && (($.eventName = PutBucketAcl) || ($.eventName = PutBucketPolicy) || ($.eventName = PutBucketCors) || ($.eventName = PutBucketLifecycle) || ($.eventName = PutBucketReplication) || ($.eventName = DeleteBucketPolicy) || ($.eventName = DeleteBucketCors) || ($.eventName = DeleteBucketLifecycle) || ($.eventName = DeleteBucketReplication)) }"
+  alarm_name = "3.8 S3 bucket policy changes"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_9" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{($.eventSource = config.amazonaws.com) && (($.eventName=StopConfigurationRecorder) || ($.eventName=DeleteDeliveryChannel) || ($.eventName=PutDeliveryChannel) || ($.eventName=PutConfigurationRecorder))}"
+  alarm_name = "3.9 AWS Config configuration changes"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_10" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventName = AuthorizeSecurityGroupIngress) || ($.eventName = AuthorizeSecurityGroupEgress) || ($.eventName = RevokeSecurityGroupIngress) || ($.eventName = RevokeSecurityGroupEgress) || ($.eventName = CreateSecurityGroup) || ($.eventName = DeleteSecurityGroup) }"
+  alarm_name = "3.10 security group changes"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_11" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventName = CreateNetworkAcl) || ($.eventName = CreateNetworkAclEntry) || ($.eventName = DeleteNetworkAcl) || ($.eventName = DeleteNetworkAclEntry) || ($.eventName = ReplaceNetworkAclEntry) || ($.eventName = ReplaceNetworkAclAssociation) }"
+  alarm_name = "3.11 changes to Network Access Control Lists (NACL)"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_12" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventName = CreateCustomerGateway) || ($.eventName = DeleteCustomerGateway) || ($.eventName = AttachInternetGateway) || ($.eventName = CreateInternetGateway) || ($.eventName = DeleteInternetGateway) || ($.eventName = DetachInternetGateway) }"
+  alarm_name = "3.12 changes to network gateways"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_13" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventName = CreateNetworkAcl) || ($.eventName = CreateNetworkAclEntry) || ($.eventName = DeleteNetworkAcl) || ($.eventName = DeleteNetworkAclEntry) || ($.eventName = ReplaceNetworkAclEntry) || ($.eventName = ReplaceNetworkAclAssociation) }"
+  alarm_name = "3.13 route table changes"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
+
+module "metric_filter_alarm_3_14" {
+  source  = "dwp/metric-filter-alarm/aws"
+  version = "1.1.0"
+
+  log_group_name = "${var.log_group_name}"
+  metric_namespace = "${var.metric_namespace}"
+  pattern = "{ ($.eventName = CreateVpc) || ($.eventName = DeleteVpc) || ($.eventName = ModifyVpcAttribute) || ($.eventName = AcceptVpcPeeringConnection) || ($.eventName = CreateVpcPeeringConnection) || ($.eventName = DeleteVpcPeeringConnection) || ($.eventName = RejectVpcPeeringConnection) || ($.eventName = AttachClassicLinkVpc) || ($.eventName = DetachClassicLinkVpc) || ($.eventName = DisableVpcClassicLink) || ($.eventName = EnableVpcClassicLink) }"
+  alarm_name = "3.14 VPC changes"
+  alarm_action_arns = "${var.alarm_action_arns}"
+}
